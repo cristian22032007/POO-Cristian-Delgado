a5b5d75db2f653a7097ea8291c8b0a0e5976f83c
cristian22032007 <cristianf.delgadoc@uqvirtual.edu.co>
2025-02-13 15:52:39 -0500

    update

---
+++ Ejercicios/src/main/java/co/edu/uniquindio/Ejercicio1.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio;
+
+public class Ejercicio1 {
+    //Imprimir los primeros 10 numeros naturales
+    public static String numerosNaturales (){
+        String numeros ="";
+        for (int i =0; i<=10; i++){
+            numeros += "El numeros es ; " + i + "\n";
+        }
+        return numeros;
+    }
+    public static void main(String[] args) {
+        System.out.println(numerosNaturales());
+    }
+}
---
+++ Ejercicios/src/main/java/co/edu/uniquindio/Ejercicio2.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio;
+
+public class Ejercicio2 {
+    public static int sumarNumeros(int numeros) {
+        int i = 1;
+        int suma = 0;
+        while (i <= numeros) {
+            suma += i;
+            i++;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumarNumeros(10));
+
+    }
+}
